c5210476d96225cac2a3251ac3ffe387b2d78703
Eric W. Bridgeford <ebridge2@jhu.edu>
2024-01-22 15:09:06 -0800

    updated test cases for cran

--- tests/testthat/test-kway_match.R
+++ tests/testthat/test-kway_match.R
@@ -27,7 +27,7 @@ test_that("as unbalancedness increases, fewer samples retained by k-way matching
   retained.high <- cb.align.kway_match(sim.high$Ts, data.frame(Covar=sim.high$Xs),
                                        match.form="Covar")
   
-  sim.mod <- cb.sims.sim_sigmoid(unbalancedness=2)
+  sim.mod <- cb.sims.sim_sigmoid(unbalancedness=1.5)
   retained.mod <- cb.align.kway_match(sim.mod$Ts, data.frame(Covar=sim.mod$Xs),
                                       match.form="Covar")
   
@@ -41,13 +41,13 @@ test_that("as unbalancedness increases, fewer samples retained by k-way matching
 
 
 test_that("K-way matching throws warning when samples retained is low", {
-  sim.low <- cb.sims.sim_sigmoid(unbalancedness=2)
+  sim.low <- cb.sims.sim_sigmoid(unbalancedness=1.5)
   expect_warning(cb.align.kway_match(sim.low$Ts, data.frame(Covar=sim.low$Xs),
-                                     match.form="Covar", retain.ratio = 0.5))
+                                     match.form="Covar", retain.ratio = 0.7))
 })
 
 test_that("K-way matching throws error when no samples retained", {
-  sim.low <- cb.sims.sim_sigmoid(unbalancedness=5)
+  sim.low <- cb.sims.sim_sigmoid(unbalancedness=10)
   expect_error(suppressWarnings(cb.align.kway_match(sim.low$Ts, data.frame(Covar=sim.low$Xs),
                                                     match.form="Covar", retain.ratio = 0.5)))
 })
